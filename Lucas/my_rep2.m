a7b896013bb62f8b642d11e5bd61464e0348d177
[LucasBolte] <l.a.b.bolte@student.tudelft.nl>
2017-01-23 16:35:04 +0100

    Better version we should not have resized the image before determining the moments

--- Lucas/my_rep2.m
+++ Lucas/my_rep2.m
@@ -4,12 +4,12 @@ function [ a ] = my_rep2( m )
 
 %% Processing
 noise_removed = opening(closing(m)); % Closed and then opened to make numbers more differentiable
-boxed = im_box(noise_removed, 0, 1); % Remove unnecessary rows, colums and make images square
+%boxed = im_box(noise_removed, 0, 1); % Remove unnecessary rows, colums and make images square
 
 %% Prepare Rotation 
-M = boxed*im_moments('central',[2 0; 1 1; 0 2]); % Determine moments
+M = noise_removed*im_moments('central',[2 0; 1 1; 0 2]); % Determine moments
 imList = [];
-num = size(boxed,1);
+num = size(noise_removed,1);
 labels = [];
 k = 0;
 featsize = [16,16];
@@ -20,16 +20,16 @@ for i=1:num;
         k = k + 1
     end
     labels = [labels;num2str(k)];
-    im = data2im(boxed(i,:));
+    im = data2im(noise_removed(i,:));
     moment = +M(i,:);
     angle = 0.5*atan((2*moment(2))/(moment(1)-moment(3)));
-    rotated_im = boxed(i,:)*im_rotate(angle);
+    rotated_im = noise_removed(i,:)*im_rotate(angle);
     binary_im = im2bw(data2im(rotated_im));
     resized_binary_im = im_resize(binary_im, featsize,'bilinear');
     imList = [imList;double(resized_binary_im(:)')];
 end
 
-csvwrite('C:\Users\TU Delf SID\code\pattern-recognition\Lucas\rotated_images2.csv',imList)
+csvwrite('C:\Users\TU Delf SID\code\pattern-recognition\Lucas\rotated_images223.csv',imList)
 a1 = prdataset(imList, labels);
 a = setfeatsize(a1, featsize);
 
