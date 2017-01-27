c725592f3c819aeedc34ac92f869ca74194ebb0b
Chantal Olieman <chantalolieman@gmail.com>
2017-01-27 18:54:01 +0100

    Wrote a big script for feature size and method

--- Lucas/featn.m
+++ Lucas/featn.m
@@ -3,33 +3,41 @@ a = rr_dataset(50, 1000);
 %forwards select different # of features
 
 %4
-[w,r] = featself(a,4);
+[w,r] = featself(a,'NN',4);
 a1 = a*w;
 m1 = errors(a1, 10);
+m = [4;m1];
+csvwrite('C:\Users\Chantal Olieman\Projects\pattern-recognition\Results\csvlist_nrfeat.csv',m)
 
 %4
-[w,r] = featself(a,8);
+[w,r] = featself(a,'NN',8);
 a2 = a*w;
 m2 = errors(a2, 10);
-
+m = [4 8;m1 m2];
+csvwrite('C:\Users\Chantal Olieman\Projects\pattern-recognition\Results\csvlist_nrfeat.csv',m)
 %4
-[w,r] = featself(a,16);
+[w,r] = featself(a,'NN',16);
 a3 = a*w;
 m3 = errors(a3, 10);
-
+m = [4 8 16;m1 m2 m3];
+csvwrite('C:\Users\Chantal Olieman\Projects\pattern-recognition\Results\csvlist_nrfeat.csv',m)
 %4
-[w,r] = featself(a,32);
+[w,r] = featself(a,'NN', 32);
 a4 = a*w;
-m4 = errors(a4, 10);
+m4 = errors(a4,10);
+m = [4 8 16 32 ;m1 m2 m3 m4];
+csvwrite('C:\Users\Chantal Olieman\Projects\pattern-recognition\Results\csvlist_nrfeat.csv',m)
 
 %64
-[w,r] = featself(a,64);
+[w,r] = featself(a,'NN',64);
 a5 = a*w;
 m5 = errors(a5, 10);
+m = [4 8 16 32 64 ;m1 m2 m3 m4 m5];
+csvwrite('C:\Users\Chantal Olieman\Projects\pattern-recognition\Results\csvlist_nrfeat.csv',m)
 
 
 %optima
-[w,r] = featself(a);
+[w,r] = featself(a, 'NN', 0);
 a6 = a*w;
 m6 = errors(a6, 10);
 
---
+++ Lucas/runboth.m
@@ -0,0 +1,2 @@
+featn
+featf
